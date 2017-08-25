49107450d2fde70e0be8997f288afde4c63b7261
vimcaw <vimcaw@gmail.com>
2017-08-25 23:51:23 +0800

    Site updated: 2017-08-25 23:51:21

--- js/post.js
+++ js/post.js
@@ -1,16 +1,24 @@
+var $imgView = $('#image-view img'),
+    $body = document.body;
+
 $$('article img').forEach(function (element) {
     element.onclick = function () {
-        if (this.className === 'original') {
-            this.className = '';
-            this.style.maxWidth = '100%';
-            return;
-        }
-        if (this.naturalWidth < this.naturalHeight) {
-        	this.style.maxWidth = '60vw';
-        	this.style.maxHeight = this.naturalHeight + 'px';
+        $imgView.src = this.src;
+        $('#image-view').removeClass('hidden');
+        $body.style.overflow = 'hidden';
+        $body.style.paddingRight = '17px';
+        if ($imgView.naturalWidth < $imgView.naturalHeight) {
+        	$imgView.style.maxWidth = '60vw';
+        	$imgView.style.maxHeight = $imgView.naturalHeight + 'px';
         } else {
-        	this.style.maxHeight = '100vh';
-        	this.style.maxWidth = this.naturalWidth + 'px';
+        	$imgView.style.maxHeight = '100vh';
+        	$imgView.style.maxWidth = $imgView.naturalWidth + 'px';
         }
     }
+})
+
+$imgView.addEventListener('click', function () {
+    $('#image-view').addClass('hidden');
+    $body.style.overflow = 'auto';
+    $body.style.paddingRight = '0';
 })
