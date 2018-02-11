16bb11c58d4c9f8e11f8db527b5c0a0d359e5c31
vimcaw <vimcaw@gmail.com>
2018-02-11 12:30:08 +0800

    Site updated: 2018-02-11 12:30:04

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,61 +1,11 @@
-var $nav = document.querySelector('.sidebar ul'),
-    $h1s = document.getElementsByTagName('h1');
-
-$h1s.forEach(function (element) {
-    let $li = document.createElement('li'),
-        $a = document.createElement('a');
-    element.id = element.innerText.replace(' ', '-');
-    $a.innerText = element.innerText;
-    $a.href = '#' + element.id;
-    $a.title = element.innerText;
-    $a.dataset.scroll = '';
-    $li.appendChild($a);
-    $nav.appendChild($li);
-    if ($a.offsetWidth > $nav.offsetWidth - 20) $a.addClass('overflow');
+hNav.init({
+    article: 'article',    //css selector of article element
+    sidebar: '.sidebar'     //css selector of sidebar element(navigation will be rendered here
 });
-
-var current = null,
-    viewHight = window.innerHeight,
-    offsetTopRate = 0.20,
-    $article = document.querySelector('article');
-
-function isOutOfTop () {
-    var offsetTop = viewHight * offsetTopRate,
-        scrollTop = document.body.scrollTop;
-    return (scrollTop + offsetTop) < $h1s[0].getPosTop();
-}
-
-function isOutOfBottom () {
-    var offsetTop = viewHight * offsetTopRate,
-        scrollTop = document.body.scrollTop;
-    return (scrollTop + offsetTop) > ($article.getPosTop() + $article.offsetHight);
-}
-
-window.addEventListener('scroll', function () {
-    var _this = this,
-        $body = document.body;
-
-    if (isOutOfTop() || isOutOfBottom()) {
-        current && current.removeClass('current');
-        current = null;
-        return;
-    }
-    $h1s.forEach(function (element) {
-        if (($body.scrollTop + viewHight * offsetTopRate) >= element.getPosTop()) {
-            if (!current || (current.innerText !== element.innerText)) {
-                current && current.removeClass('current');
-                current = $('a[title="' + element.innerText + '"]');
-                current && current.addClass('current');
-            }
-        } else {
-            return false;
-        }
-    })
-})
 
 window.addEventListener('scroll', function () {
     const backRate = 0.2;
-    if (document.body.scrollTop / document.body.scrollHeight >= 0.2) {
+    if (document.documentElement.scrollTop / document.body.scrollHeight >= backRate) {
         $('#back-to-top').style.display = 'block';
     } else {
         $('#back-to-top').style.display = 'none';
